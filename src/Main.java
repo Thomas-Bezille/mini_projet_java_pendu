d2e5229e956ef682f935a87d43607f5507dfe2a7
thomas <bezille.thomasb@gmail.com>
2024-07-22 11:59:38 +0200

    Ability to replay or exit the program

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,44 @@ public class Main {
 
         final var random = new Random();
         final var words = "fleur voiture océan montagne livre soleil arbre chat téléphone maison".split(" ");
-        final var wordToGuess = words[random.nextInt(words.length)];
-        final var game = new GuessGame(wordToGuess, 10);
+        var wordToGuess = words[random.nextInt(words.length)];
+        var game = new GuessGame(wordToGuess, 10);
 
         System.out.println("Le jeu débute !");
 
         while (true) {
             System.out.println(game);
-            final var letter = scanLetter();
+            final var letter = scanLetter("Entrez une lettre :");
 
             game.guessLetter(letter);
 
             if (game.isLost()) {
                 System.out.println(game);
                 System.out.println("Perdu !");
-                break;
             }
 
             if (game.isWin()) {
                 System.out.println(game);
                 System.out.println("Gagné !");
-                break;
+            }
+
+            if (game.isWin() || game.isLost()) {
+                var replayAnswer = scanLetter("Voulez-vous rejouer ? (O/N)");
+                if (replayAnswer == 'o' || replayAnswer == 'O') {
+                    wordToGuess = words[random.nextInt(words.length)];
+                    game = new GuessGame(wordToGuess, 10);
+                } else {
+                    break;
+                }
             }
         }
     }
 
-    private static char scanLetter() {
+    private static char scanLetter(String question) {
         final var scanner = new Scanner(System.in);
         Character letter = null;
         do {
-            System.out.println("Entrez une lettre :");
+            System.out.println(question);
             var input = scanner.nextLine();
 
             if (input.length() == 1) {
